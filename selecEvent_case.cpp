1d11e5498d49c1e509764084616d71c3ec0d7a6e
Oscar Altuve MV <altuveoscar95@gmail.com>
2020-08-30 22:51:33 +0200

    Actualización

--- selecEvent_case.cpp
+++ selecEvent_case.cpp
@@ -8,11 +8,11 @@ TString selecEvent(int option,int &s_size,int &i,int size){
         case 0:{
             TString sample[] = {"data","ZqqZll","WqqZll","WpqqWmlv","WplvWmqq","WlvZqq","llll","lllv","llvv","lvvv","Zee","Zmumu","Ztautau","single_top_tchan","single_antitop_tchan","single_top_schan","single_antitop_schan","single_top_wtchan","single_antitop_wtchan","ttbar_lep","Wplusenu","Wplusmunu","Wplustaunu","Wminusenu","Wminusmunu","Wminustaunu","ggH125_ZZ4lep","ZH125_ZZ4lep","WH125_ZZ4lep","VBFH125_ZZ4lep"};
             s_size = sizeof(sample)/sizeof(sample[0]);
-                aux=sample[i];
-                break;
+            aux=sample[i];
+            break;
         }
         case 1:{
-            TString sample[] = {"data"};
+            TString sample[] = {"data",};
             s_size = sizeof(sample)/sizeof(sample[0]);
             aux=sample[i];
             break;
